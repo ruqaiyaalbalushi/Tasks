001cdf8ce047458b06097dc7ac4369821d818f77
ruqaiyaalbalushi <ruqaiya099@gmail.com>
2023-11-15 16:42:50 +0400

    exercise

---
+++ problem of the day3.py
@@ -0,0 +1,10 @@
+
+arr1=[1,3,5,7]
+arr2=[0,2,6,8,9]
+merged_array = arr1 + arr2
+print (*sorted(merged_array))
+
+arr3=[10,12]
+arr4=[5,18,20]
+merged_array2 = arr3 + arr4
+print (*sorted(merged_array2))
