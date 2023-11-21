6132469e4488448622d527ed121d939f0a61a406
ruqaiyaalbalushi <ruqaiya099@gmail.com>
2023-11-21 14:51:22 +0400

    exercise

---
+++ 21-11-2023.py
@@ -0,0 +1,163 @@
+#chenge the arhemetic opertion to python import math
+"""n=input("Enter n")
+s=input("Enter s")
+
+def calc(n ,s):
+    
+    area=0
+    pi=3.14
+    area = n*s^2/4*math.tan(pi/n)
+    return area
+print(area)"""
+    
+
+"""n=input("write a word")
+v="aiuo"
+def word (sting):
+    for litter in n:
+        if(word in v):
+            print(word.upper())"""
+    
+    
+    
+    
+"""list1=[8,1,5,4]
+for i in range (len(list1)):
+    print(list1[i])
+print('------------')
+for i in range(2,4):
+    print(i)"""
+
+#print list as its:
+"""list1=[8,11,5,4]
+print(list1)"""
+
+"""s="ruqaiya albalushi"
+list1=s.split(" ")
+print(list1)"""
+#copiny s to x
+"""s=[2,5,5,1,7]
+x=list(s)
+print(x)
+x[4]=5
+print(x)
+print(s)"""
+#or
+"""s=[2,5,5,1,7]
+x=s[:]
+print(x)
+x[4]=5
+print(x)
+print(s)"""
+#or
+"""s=[2,5,5,1,7]
+x=s.copy()
+print(x)
+x[4]=5
+print(x)
+print(s)"""
+
+"""x=[1,2,3]
+list1=x.split(" ")
+print(list1)"""
+"""s=[1,2,3]
+for i in s:
+    print(i, end=" ")"""
+"""s=[1,2,3]
+for i in s:
+    print(i*2, end=" ")"""
+#couting nagetive number:
+"""s=[-1,2,3,-5,6]
+count=0
+for i in s:
+    if(i<0):
+        count+=1
+print(count)"""
+#append:
+
+"""s=[-2,9,6,-6,-7]
+s.append(3)
+print(s)
+s.insert(0,3)
+print(s)
+s.pop()
+print(s)
+
+s.remove(6)
+print(s)"""
+
+"""first_name=["ruqaiya"]
+last_name=["albalushi"]
+full_name=first_name+last_name
+print(first_name)
+print(last_name)
+print(full_name)"""
+
+
+
+"""list1=[]
+while(True):
+    n=input("Enter a number")
+    if(n!='Q'):
+        list1+=n
+    else:
+        break
+    
+        
+    
+print(list1)"""
+"""list1=[33,9,0,99,101,201]
+list2=[]
+for i in list1:
+    if (i > 100):
+        list2.append(i)
+        print(i,"position is",list1.index(max(list2)))
+        break"""
+      
+n=int(input("Enter number"))
+def main():
+    
+    
+def side ():
+    s=input("Enter the number of sides ")
+def count():
+
+list1=[]
+while(True):
+    if(n<=6):
+        list1.append(n)
+        
+        
+    else:
+        break
+print(list1)
+    
+    
+        
+    
+    
+    
+    
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
