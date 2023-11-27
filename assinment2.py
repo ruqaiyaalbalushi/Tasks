7dda8b5ff77c24ddb64efaea424b4151e3faa261
ruqaiyaalbalushi <150660657+ruqaiyaalbalushi@users.noreply.github.com>
2023-11-27 22:34:59 +0400

    Add files via upload

---
+++ assinment2.py
@@ -0,0 +1,130 @@
+#Question 1:
+"""Values = [0, 0, 0, 0, 0, 0, 0]
+Values[0]=10
+Values[6]=10
+print(Values)
+
+#Question 2:
+
+x=[6,3,8,1,7]
+a=x[::-1]
+print(a)
+
+
+
+#Question 3:
+color=['red', 'yellow','pink','black']
+color.append('grean')
+color.insert(1,'purple')
+color.remove('pink')
+print(color)"""
+
+
+
+
+
+#Question 4:
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+print(fruits.count("apple"))
+print(fruits.count("strawberry"))
+print(fruits.index("banana"))
+print(fruits.index("banana", 4))
+fruits.reverse()
+fruits.append("grape")
+fruits.sort()
+fruits.pop()
+print(fruits)
+
+#Question 5:
+"""x=[23,54,76,12,90]
+for i in x:
+ print(i, end="|")"""
+
+
+#Question 6:
+
+
+"""list1=[1,0,0,10,0,0,0]
+count=0
+for i in list1:
+    if(i==0):
+        count+=1
+print(count)"""
+
+
+
+#Question 7:
+"""d = "a*hj"
+list(d)
+print(list(d))"""
+
+
+
+
+
+
+#Question 8:
+
+"""b= ['p', 'r', 'a', 'c', 't', 'i', 'c', 'e']
+for i in b:
+ print(i, end="?")"""
+#Question 9:
+
+"""b = "Hello World"
+a = list(b)
+print(a)
+print(len(a))
+print(a[1:11])
+print(a[-2:-5:-1])
+print(a[:2])
+print(a[:4])
+print(a[4:])"""
+
+
+
+
+#Question 10:
+
+
+
+
+"""def longer_word(lst, n):
+    result=[]
+    for i in lst:
+        if(len(i) > n):
+            result.append(i)
+    
+    return result
+
+
+word = input("Enter a list of words separated by space: ")
+words = word.split()
+
+n = int(input("Enter the minimun number : "))
+
+longer_words = longer_word(words, n)
+print("Words longer than", n, "are:", longer_words)"""
+
+
+
+
+
+
+
+
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
